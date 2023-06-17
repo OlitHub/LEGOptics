77b3118092297a8e12e1003a10e0dec170af5727
VladMrchd <vladimir.marchand.pro@gmail.com>
2023-06-17 20:37:46 +0200

    voice

--- app/src/main/java/com/example/temp/MainActivity.kt
+++ app/src/main/java/com/example/temp/MainActivity.kt
@@ -5,12 +5,16 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.speech.RecognitionListener
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.util.Log
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +22,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var editTextPseudo: EditText
     private lateinit var editTextPassword: EditText
     private lateinit var preferences: SharedPreferences
+
+    private lateinit var speechRecognizer: SpeechRecognizer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,6 +64,72 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Mauvaise connexion", Toast.LENGTH_SHORT).show()
             }
         }
+
+
+
+        val voiceButton: Button = findViewById(R.id.voiceButton)
+
+        // Configurez le bouton pour écouter le clic et activer la reconnaissance vocale
+        voiceButton.setOnClickListener {
+            startSpeechRecognition()
+            Log.i("MainActivity", "Bouton de la voix cliqué")
+        }
+
+        // Initialisez SpeechRecognizer
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        Log.i("MainActivity", "Bouton de la voix cliqué")
+
+
+
+        speechRecognizer.setRecognitionListener(object : RecognitionListener {
+            override fun onReadyForSpeech(params: Bundle?) {
+                // La reconnaissance vocale est prête à écouter
+            }
+
+            override fun onBeginningOfSpeech() {
+                // Le début de l'entrée vocale a été détecté
+            }
+
+            override fun onRmsChanged(rmsdB: Float) {
+                // Le niveau de signal audio a changé
+            }
+
+            override fun onBufferReceived(buffer: ByteArray?) {
+                // Les données audio sont en cours de réception
+            }
+
+            override fun onEndOfSpeech() {
+                // La fin de l'entrée vocale a été détectée
+            }
+
+            override fun onError(error: Int) {
+                // Une erreur s'est produite lors de la reconnaissance vocale
+            }
+
+            override fun onResults(results: Bundle?) {
+                // Les résultats de la reconnaissance vocale sont disponibles
+                val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+
+                if (!matches.isNullOrEmpty()) {
+                    val voiceCommand = matches[0]
+                    if (voiceCommand.equals("Connexion", ignoreCase = true)) {
+                        // Lancer l'activité ListMan
+                        val intent = Intent(this@MainActivity, ListMan::class.java)
+                        startActivity(intent)
+                    }
+                }
+            }
+
+            override fun onPartialResults(partialResults: Bundle?) {
+                // Les résultats partiels de la reconnaissance vocale sont disponibles
+            }
+
+            override fun onEvent(eventType: Int, params: Bundle?) {
+                // Des événements supplémentaires sont disponibles
+            }
+        })
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -79,4 +151,25 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+
+    private fun startSpeechRecognition() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+
+        // Définissez la langue de reconnaissance vocale si nécessaire
+        // intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "fr-FR")
+
+        // Lancez la reconnaissance vocale
+        speechRecognizer.startListening(intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Libérez les ressources de SpeechRecognizer
+        speechRecognizer.destroy()
+    }
+
+
 }
